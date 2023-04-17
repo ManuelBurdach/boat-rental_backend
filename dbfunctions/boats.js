deb30360e7934a197065c3c9ae21d85b3cbe1790
Manuel Burdach <manuelburdach@outlook.de>
2023-04-17 11:56:13 +0200

    Update V1

--- dbfunctions/boats.js
+++ dbfunctions/boats.js
@@ -6,7 +6,6 @@ import { ObjectId } from "mongodb";
 const COL_BOATS = process.env.COL_BOATS;
 
 // -------------------------------------------- DB FUNCTIONS
-
 // -------------------------------------------- GET BOATS
 export const getBoats = async (req, res) => {
   try {
@@ -30,6 +29,7 @@ export const addBoat = async (req, res) => {
       serialNumber: req.body.serialNumber,
       material: req.body.material,
       bootType: req.body.bootType,
+      user: req.body.user,
     });
     if (result) return res.status(200).json(result);
     else {
@@ -66,6 +66,7 @@ export const editBoat = async (req, res) => {
           serialNumber: req.body.serialNumber,
           material: req.body.material,
           bootType: req.body.bootType,
+          user: req.body.user,
         },
       }
     );
@@ -77,3 +78,41 @@ export const editBoat = async (req, res) => {
     return res.status(500).end();
   }
 };
+
+// -------------------------------------------- EXECUTE ONCE (PUT COLLECTION WITH VALIDATION & SCHEMA)
+const putCol = async () => {
+  const db = await dbConnect();
+  const result = await db.createCollection(COL_BOATS, {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Boat Object Validation",
+        required: ["constructionYear", "serialNumber", "material", "bootType", "user"],
+        properties: {
+          constructionYear: {
+            bsonType: "string",
+            description: "'constructionYear' must be a string and is required",
+          },
+          serialNumber: {
+            bsonType: "string",
+            description: "'serialNumber' must be a string and is required",
+          },
+          material: {
+            bsonType: "string",
+            description: "'material' must be a string and is required",
+          },
+          bootType: {
+            bsonType: "string",
+            description: "'bootType' must be a string and is required",
+          },
+          user: {
+            bsonType: "string",
+            description: "'user' must be a string and is required",
+          },
+        },
+      },
+    },
+  });
+};
+
+// putCol();
